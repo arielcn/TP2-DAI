447a452f38f0a0da1e3e354ce005865ecab85d20
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-30 12:10:25 -0300

    TERMINADO

--- componentes/Card.js
+++ componentes/Card.js
@@ -2,21 +2,30 @@ import React from 'react';
 import {View, Image, StyleSheet, Text} from 'react-native';
 
 const styles = StyleSheet.create({
-  tinyLogo: {
-    width: 200,
+    container: {
+        backgroundColor: 'green',
+        borderRadius: 50,
+        marginTop: 20,
+    },
+  cafe: {
+    width: 280,
     height: 300,
-    borderRadius: 10,
+    borderRadius: 50,
   },
   texto: {
     overflow: 'hidden',
     textAlign: 'center',
+    padding: 10.,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
   }
 });
 
 const Card = () => {
   return (
     <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'}}/>
+      <Image style={styles.cafe} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'}}/>
       <Text style={styles.texto}>Americano</Text>
     </View>
   );
